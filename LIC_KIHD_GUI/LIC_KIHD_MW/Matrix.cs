55175431d5675ad94736e86e45b281500506514b
Chao Wang <cwang7@uwsuper.edu>
2018-03-28 02:45:37 -0500

    class frame

---
+++ LIC_KIHD_GUI/LIC_KIHD_MW/Matrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIC_KIHD_MW
+{
+    class Matrix
+    {
+        private int column;
+        private int row;
+        private double[,] data;
+        public Matrix(int theRow, int theCol)
+        {
+            row = theRow;
+            column = theCol;
+            data = new double[row, column];
+        }
+        public void addData(int row, int col, double data)
+        {
+
+        }
+        public Matrix multiplication(Matrix m, Matrix n)
+        {
+            Matrix value = new Matrix();
+            return value;
+        }
+        public Matrix transpose(Matrix x)
+        {
+            return x;
+        }
+    }
+}
---
+++ LIC_KIHD_GUI/LIC_KIHD_MW/Policy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIC_KIHD_MW
+{
+    class Policy
+    {
+        private string policyNum;
+        private double payoffAmount;
+        private Agent agent;
+        private double duration;
+        private Beneficiary beneficiary;
+        private double fartherDeathAge;
+        private double motherDeathAge;
+        private double cigsPerDay;
+        private double smokingHistory;
+        private double bloodPressure;
+        private double gramsFatPerDay;
+        private Boolean heartDisease;
+        private Boolean cancer;
+        private Boolean hospitalized;
+        private string dangerousAct;
+        public Policy()
+        {
+
+        }
+        public void cancel(Policy policy)
+        {
+
+        }
+        public void makeClaim(Policy policy)
+        {
+
+        }
+        private Report SendReport(List<Policy> policy)
+        {
+            Report rept = new Report();
+            return rept;
+        }
+        public double calculatePremium(Policy policy)
+        {
+            double result;
+            return result;
+        }
+        private double predictAgeAtDeath(Matrix D, Matrix y)
+        {
+            double age;
+            return age;
+        }
+    }
+}
