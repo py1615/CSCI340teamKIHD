61ce7665d0ed6d3499ee92ee0ce0121398d11895
Noi3skill <Noi3skill@gmail.com>
2018-04-01 08:13:11 -0500

    added login procedure - need access to db

--- DATABASE/LIC_db_setup.sql
+++ DATABASE/LIC_db_setup.sql
@@ -1,50 +1,85 @@
-create table employee (
-ID varchar(20) NOT NULL,
-first_name varchar(100),
-last_name varchar(100),
-user_type varchar(1),
-department varchar(50),
-employee_password varchar(255),
-PRIMARY KEY (ID)
+CREATE TABLE employee (
+id VARCHAR(20) NOT NULL,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+user_type VARCHAR(1),
+department VARCHAR(50),
+employee_password VARCHAR(255),
+PRIMARY KEY (id)
 );
 
-create table payments (
-policy_number varchar(30) NOT NULL,
-date_paid datetime NOT NULL,
-amount varchar(10),
-payment_type varchar(1),
-CONSTRAINT PK_payments PRIMARY KEY (policy_number, date_paid),
-FOREIGN KEY date_paid REFERENCES client_policy(payments)
-);
-
-create table client_policy (
-policy_number varchar(30) NOT NULL,
-first_name varchar(100),
-last_name varchar(100),
-dob date,
-fathers_aod date,
-mothers_aod date,
-cigs_day varchar(5),
-smoking_history date,
-sbp varchar(4),
-avg_grams_fat_day varchar(5),
-heart_disease varchar(1),
-cancer varchar(1),
-hospitalized varchar(1),
-dangerous_activities varchar(255),
-policy_start date,
-policy_end date,
-agent_id varchar(20),
-payoff_amount varchar(10),
-monthly_premium varchar(10),
+CREATE TABLE client_policy (
+policy_number VARCHAR(30) NOT NULL,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+dob DATE,
+fathers_aod VARCHAR(5),
+mothers_aod VARCHAR(5),
+cigs_day VARCHAR(5),
+smoking_history DATE,
+sbp VARCHAR(4),
+avg_grams_fat_day VARCHAR(5),
+heart_disease VARCHAR(1),
+cancer VARCHAR(1),
+hospitalized VARCHAR(1),
+dangerous_activities VARCHAR(255),
+policy_start DATE,
+policy_end DATE,
+agent_id VARCHAR(20),
+payoff_amount VARCHAR(10),
+monthly_premium VARCHAR(10),
 PRIMARY KEY (policy_number),
-FOREIGN KEY agent_id REFERENCES employee(agent_id)
+FOREIGN KEY (agent_id) REFERENCES employee(id)
 );
 
-create table beneficiary (
-policy_number varchar(20) NOT NULL,
-first_name varchar(100),
-last_name varchar(100),
+CREATE TABLE deliquent (
+policy_number VARCHAR(30) NOT NULL,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+dob DATE,
+fathers_aod VARCHAR(5),
+mothers_aod VARCHAR(5),
+cigs_day VARCHAR(5),
+smoking_history DATE,
+sbp VARCHAR(4),
+avg_grams_fat_day VARCHAR(5),
+heart_disease VARCHAR(1),
+cancer VARCHAR(1),
+hospitalized VARCHAR(1),
+dangerous_activities VARCHAR(255),
+policy_start DATE,
+policy_end DATE,
+agent_id VARCHAR(20),
+payoff_amount VARCHAR(10),
+monthly_premium VARCHAR(10),
+PRIMARY KEY (policy_number),
+FOREIGN KEY (agent_id) REFERENCES employee(id)
+);
+
+CREATE TABLE payments (
+policy_number VARCHAR(30) NOT NULL,
+date_paid DATETIME NOT NULL,
+amount VARCHAR(10),
+payment_type VARCHAR(1),
+CONSTRAINT PK_payments PRIMARY KEY (policy_number, date_paid),
+FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number)
+);
+
+CREATE TABLE beneficiary (
+policy_number VARCHAR(30) NOT NULL,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
 PRIMARY KEY (policy_number),
 FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number)
+);
+
+CREATE TABLE policy_holder (
+policy_holder_id VARCHAR(30) NOT NULL,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+street_address VARCHAR(30),
+city_address VARCHAR(20),
+state_address VARCHAR(2),
+zip_address VARCHAR(9),
+PRIMARY KEY (policy_holder_id)
 );
