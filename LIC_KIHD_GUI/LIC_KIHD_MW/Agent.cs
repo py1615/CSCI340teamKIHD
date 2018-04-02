8bebf6b74de363f4a0601f0610075a7a19122c13
Lee,Heejung <hlee20@uwsuper.edu>
2018-04-01 21:35:07 -0500

    updated login method in agent class for chao

--- LIC_KIHD_GUI/LIC_KIHD_MW/Agent.cs
+++ LIC_KIHD_GUI/LIC_KIHD_MW/Agent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace LIC_KIHD_MW
 {
@@ -20,9 +21,34 @@ namespace LIC_KIHD_MW
             Policy client = new Policy();
             return client;
         }
-        public void login(string userName, string passWord)
+        public static Boolean login(string userName, string passWord)
         {
+            String connectionString = Properties.Settings.Default.SQLConnection;
+            SqlConnection conn = new SqlConnection(connectionString);
+            //String query = "";
 
+            //insert catch here
+
+            //SqlCommand cmd = new SqlCommand(query);
+            //cmd.Connection = conn;
+            //conn.Open();
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+            String query = "execute get_login '" + userName + "', '" + passWord + "'";
+            SqlCommand command = new SqlCommand(query);
+            command.Connection = conn;
+            conn.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                conn.Close();
+                return true;
+            } else
+            {
+                conn.Close();
+                return false;
+            }
         }
         public void logout()
         {
