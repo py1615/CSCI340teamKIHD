8c4b3cef544e3ecf13d199f40c7f8bc94a90a0b1
Noi3skill <37150888+Noi3skill@users.noreply.github.com>
2018-04-09 09:11:04 -0500

    Update LIC_db_stored_procedures.sql

--- DATABASE/LIC_db_stored_procedures.sql
+++ DATABASE/LIC_db_stored_procedures.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE login (@id varchar(30), @password varchar(255))
 AS
 BEGIN
 SET NOCOUNT ON;
-SELECT id, employee_password
+SELECT id, employee_password, user_type
 FROM dbo.employee
 WHERE id = @id and employee_password = @password
 END
