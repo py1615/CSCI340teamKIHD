137b2d70f6c7962ccda8a8cb85181305c3d37a96
Noi3skill <37150888+Noi3skill@users.noreply.github.com>
2018-04-12 18:02:06 -0500

    Update LIC_db_stored_procedures.sql

--- DATABASE/LIC_db_stored_procedures.sql
+++ DATABASE/LIC_db_stored_procedures.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE login (@id varchar(20), @password varchar(255))
+CREATE PROCEDURE get_login (@id varchar(20), @password varchar(255))
 AS
 BEGIN
 SET NOCOUNT ON;
@@ -37,31 +37,16 @@ WHERE policy_number = @policy_number
 END
 GO
 
-CREATE PROCEDURE cancel (@policy_number varchar(30))
+CREATE PROCEDURE register_user (@policy_number varchar(30))
 AS
 BEGIN
 SET NOCOUNT ON;
-INSERT INTO dbo.delinquent (policy_number,
+INSERT INTO dbo.employee (id,
 first_name,
 last_name,
-dob,
-fathers_age_of_death,
-mothers_afe_of_death,
-cigs_day,
-smoking_history,
-systolic_blood_pressure,
-avg_grams_fat_day,
-heart_disease,
-cancer,
-hospitalized,
-dangerous_activities,
-policy_start,
-policy_end,
-agent_id,
-payoff_amount,
-monthly_premium)
-SELECT *
-FROM dbo.client_policy
-WHERE policy_number = @policy_number
+user_type,
+department,
+employee_password)
+
 END
 GO
