99819d4a73b0176040607cff7a240016a7804828
Noi3skill <Noi3skill@gmail.com>
2018-04-25 21:38:45 -0500

    added register_policy

--- DATABASE/LIC_db_setup.sql
+++ DATABASE/LIC_db_setup.sql
@@ -1,3 +1,9 @@
+--drop table beneficiary
+--drop table payments
+--drop table client_policy
+--drop table policy_holder
+--drop table employee
+
 CREATE TABLE employee (
 id NUMERIC(20) NOT NULL IDENTITY PRIMARY KEY,
 username varchar(20),
@@ -5,8 +11,7 @@ first_name VARCHAR(100),
 last_name VARCHAR(100),
 employee_password VARCHAR(255),
 user_type VARCHAR(1),
-department VARCHAR(50)
-);
+department VARCHAR(50));
 
 CREATE TABLE policy_holder (
 policy_holder_id NUMERIC(30) NOT NULL IDENTITY PRIMARY KEY,
@@ -15,8 +20,7 @@ last_name VARCHAR(100),
 street_address VARCHAR(30),
 city_address VARCHAR(20),
 state_address VARCHAR(2),
-zip_address VARCHAR(9),
-);
+zip_address VARCHAR(9));
 
 CREATE TABLE client_policy (
 policy_number NUMERIC(30) NOT NULL IDENTITY PRIMARY KEY,
@@ -39,8 +43,7 @@ payoff_amount NUMERIC(10,2),
 monthly_premium NUMERIC(10,2),
 policy_status VARCHAR(1),
 FOREIGN KEY (policy_holder_id) REFERENCES policy_holder(policy_holder_id),
-FOREIGN KEY (agent_id) REFERENCES employee(id)
-);
+FOREIGN KEY (agent_id) REFERENCES employee(id));
 
 CREATE TABLE payments (
 date_paid DATETIME NOT NULL,
@@ -48,13 +51,11 @@ policy_number NUMERIC(30) NOT NULL,
 amount NUMERIC(10,2),
 payment_type VARCHAR(1),
 CONSTRAINT PK_payments PRIMARY KEY (policy_number, date_paid),
-FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number)
-);
+FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number));
 
 CREATE TABLE beneficiary (
 policy_number NUMERIC(30) NOT NULL,
 first_name VARCHAR(100),
 last_name VARCHAR(100),
 PRIMARY KEY (policy_number),
-FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number)
-);
+FOREIGN KEY (policy_number) REFERENCES client_policy(policy_number));
