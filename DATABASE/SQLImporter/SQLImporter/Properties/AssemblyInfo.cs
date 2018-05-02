d227e424ada88f24710b8a4e2bdfe23256deaf61
Chao Wang <cwang7@uwsuper.edu>
2018-05-01 23:44:22 -0500

    recompiled code

--- DATABASE/SQLImporter/SQLImporter/Properties/AssemblyInfo.cs
+++ DATABASE/SQLImporter/SQLImporter/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SQLImporter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLImporter")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e657e3f4-257f-47ff-8e08-7de99051a1e9")]
+[assembly: Guid("cd7693e0-477e-4318-9a5d-feb2018f5a58")]
 
 // Version information for an assembly consists of the following four values:
 //
--- DATABASE/SQLImporter/SQLImporter/Properties/Settings.Designer.cs
+++ DATABASE/SQLImporter/SQLImporter/Properties/Settings.Designer.cs
@@ -8,38 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SQLImporter.Properties {
-    
-    
+namespace SQLImporter.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Initial Catalog=Restaurant;Data Source=SROSEN-LT-5000;\" + \"Integrated Security=Fa" +
-            "lse;user=\'middleware\';pwd=\'password\'")]
-        public string SQLConnectionRosen {
-            get {
-                return ((string)(this["SQLConnectionRosen"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DATABASE\\CSCI3400011030;Initial Catalog=LIC_KIHD;Integrated Security=" +
-            "false;user=\'LIC_KIHD_MW\';pwd=\'KIHD\';")]
-        public string SQLConnection {
-            get {
-                return ((string)(this["SQLConnection"]));
             }
         }
     }
