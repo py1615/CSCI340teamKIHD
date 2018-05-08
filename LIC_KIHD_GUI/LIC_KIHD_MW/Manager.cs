20a8f8e99e7a798ec5d3604cb145eff1c2c0fa45
Chao Wang <cwang7@uwsuper.edu>
2018-05-08 17:37:45 -0500

    fix

--- LIC_KIHD_GUI/LIC_KIHD_MW/Manager.cs
+++ LIC_KIHD_GUI/LIC_KIHD_MW/Manager.cs
@@ -33,7 +33,7 @@ namespace LIC_KIHD_MW
             string id = "";
             while (reader.Read())
             {
-                id = "" + reader.GetString(reader.GetOrdinal("agent_id"));
+                id = "" + reader.GetString(reader.GetOrdinal("id"));
             }
             conn.Close();
             return id;
